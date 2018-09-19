02861e04b6168316590d1c75211f7ea07fdc52ab
Ana Mafla <anamafla@hotmail.com>
2018-09-19 17:50:17 -0500

    Route Protection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter  as Router, Route, Switch} from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import store from './store';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
 
 import './App.css';
 import AppNavbar from './components/layout/AppNavbar';
@@ -20,11 +22,11 @@ class App extends Component {
         <AppNavbar/>
         <div className="container">
           <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/client/add" component={AddClient} />
-            <Route exact path="/client/edit/:id" component={EditClient} />
-            <Route exact path="/client/:id" component={ClientDetails} />
+            <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+            <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
+            <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+            <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+            <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
           </Switch>
         </div>
       </div>
